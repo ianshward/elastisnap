8f194c86d8dc6e54ea58f62ed39acdfb0dd1a2cb
Ian Ward <ianshward@gmail.com>
2011-11-18 16:01:56 -0500

    indentation, type

--- index.js
+++ index.js
@@ -31,49 +31,49 @@ ec2 = aws.createEC2Client(options.awskey, options.awssecret, {version: '2010-08-
 
 var jobs = options.jobs;
 
-function run(selfInstanceId) {
 // This does the following:
 //   - Gets the volume-id based on device + instance-id
 //   - Creates a snapshot based on that volume-id
 //   - Deletes the oldest snapshot of the pool size is exceeded
-_.each(jobs, function(job, key) {
-    var id = key == 'self' ? selfInstnaceId : key;
-    var devices = job.devices.split(/\s*,\s*/);
-    _.each(devices, function(device) {
-        var params = {};
-        params['Filter.1.Name'] = 'attachment.device';
-        params['Filter.1.Value.1'] = device;
-        params['Filter.2.Name'] = 'attachment.instance-id';
-        params['Filter.2.Value.1'] = key;
-        ec2.call('DescribeVolumes', params, function(result) {
-            console.log(result.volumeSet.item.volumeId);
-            var volume = result.volumeSet.item.volumeId;
-            var description = job.description + ' ' + device + ' ' + id;
-            ec2.call('CreateSnapshot', {VolumeId: volume, Description: description}, function(result) {
-                if (!result.Errors) {
-                    var params = {};
-                    params['Owner'] = 'self';
-                    params['Filter.1.Name'] = 'description';
-                    params['Filter.1.Value.1'] = description;
-                    ec2.call("DescribeSnapshots", params, function(result) {
-                        if (!result.Errors) {
-                            if (result.snapshotSet.item) {
-                                var snapshots = result.snapshotSet.item;
-                                if (snapshots.length > job.pool) {
-                                    snapshots = _.sortBy(snapshots, function(snapshot) { return new Date(snapshot.startTime).getTime(); });
-                                    // Delete oldest snapshot within the pool. Assumes pool is not already exceeded.
-                                    ec2.call('DeleteSnapshot', {SnapshotId: snapshots[0].snapshotId}, function(result) {
-                                        // Do nothing
-                                    });
+function run(selfInstanceId) {
+    _.each(jobs, function(job, key) {
+        var id = key == 'self' ? selfInstanceId : key;
+        var devices = job.devices.split(/\s*,\s*/);
+        _.each(devices, function(device) {
+            var params = {};
+            params['Filter.1.Name'] = 'attachment.device';
+            params['Filter.1.Value.1'] = device;
+            params['Filter.2.Name'] = 'attachment.instance-id';
+            params['Filter.2.Value.1'] = key;
+            ec2.call('DescribeVolumes', params, function(result) {
+                console.log(result.volumeSet.item.volumeId);
+                var volume = result.volumeSet.item.volumeId;
+                var description = job.description + ' ' + device + ' ' + id;
+                ec2.call('CreateSnapshot', {VolumeId: volume, Description: description}, function(result) {
+                    if (!result.Errors) {
+                        var params = {};
+                        params['Owner'] = 'self';
+                        params['Filter.1.Name'] = 'description';
+                        params['Filter.1.Value.1'] = description;
+                        ec2.call("DescribeSnapshots", params, function(result) {
+                            if (!result.Errors) {
+                                if (result.snapshotSet.item) {
+                                    var snapshots = result.snapshotSet.item;
+                                    if (snapshots.length > job.pool) {
+                                        snapshots = _.sortBy(snapshots, function(snapshot) { return new Date(snapshot.startTime).getTime(); });
+                                        // Delete oldest snapshot within the pool. Assumes pool is not already exceeded.
+                                        ec2.call('DeleteSnapshot', {SnapshotId: snapshots[0].snapshotId}, function(result) {
+                                            // Do nothing
+                                        });
+                                    }
                                 }
                             }
-                        }
-                    });
-                }
+                        });
+                    }
+                });
             });
         });
     });
-});
 }
 
 function getInstanceId(cb) {
