b4f14e7b7305db64415aac4625d2ffffcb99874e
Ian Ward <ianshward@gmail.com>
2012-07-07 09:05:08 -0400

    accept cli arguments; pass in both err and result to aws-lib ec2.call

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ try {
    console.warn('Invalid JSON config file: ' + options.config);
    throw e;
 }
-
+// Allow options command-line overrides
+_.each(argv, function(v, k) {
+    options[k] = argv[k] || options[k];
+});
 var jobs;
 // Allow a single job to be passed in as cli args
 var single = {};
@@ -66,23 +69,23 @@ function run(selfInstanceId) {
             params['Filter.1.Value.1'] = device;
             params['Filter.2.Name'] = 'attachment.instance-id';
             params['Filter.2.Value.1'] = id;
-            ec2.call('DescribeVolumes', params, function(result) {
+            ec2.call('DescribeVolumes', params, function(err, result) {
                 var volume = result.volumeSet.item.volumeId;
                 var description = job.description + ' ' + device + ' ' + id;
-                ec2.call('CreateSnapshot', {VolumeId: volume, Description: description}, function(result) {
+                ec2.call('CreateSnapshot', {VolumeId: volume, Description: description}, function(err, result) {
                     if (!result.Errors) {
                         var params = {};
                         params['Owner'] = 'self';
                         params['Filter.1.Name'] = 'description';
                         params['Filter.1.Value.1'] = description;
-                        ec2.call("DescribeSnapshots", params, function(result) {
+                        ec2.call("DescribeSnapshots", params, function(err, result) {
                             if (!result.Errors) {
                                 if (result.snapshotSet.item) {
                                     var snapshots = result.snapshotSet.item;
                                     if (snapshots.length > job.pool) {
                                         snapshots = _.sortBy(snapshots, function(snapshot) { return new Date(snapshot.startTime).getTime(); });
                                         // Delete oldest snapshot within the pool. Assumes pool is not already exceeded.
-                                        ec2.call('DeleteSnapshot', {SnapshotId: snapshots[0].snapshotId}, function(result) {
+                                        ec2.call('DeleteSnapshot', {SnapshotId: snapshots[0].snapshotId}, function(err, result) {
                                             // Do nothing
                                         });
                                     }
