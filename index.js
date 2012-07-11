89d7e0164883252be9f8f684c5a68f4ede000178
Will White <will@mapbox.com>
2012-07-11 00:48:58 +0000

    More error reporting.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var aws = require("aws-lib"),
-    _ = require("underscore"),
-    argv = require('optimist').argv,
-    fs = require('fs'),
-    exec = require('child_process').exec;
+var aws = require("aws-lib");
+var _ = require("underscore");
+var argv = require('optimist').argv;
+var fs = require('fs');
+var exec = require('child_process').exec;
+var Step = require('step');
 
 if (!argv.config) {
     console.log("Must provide --config argument which points to json settings file, such as --config settings.json");
@@ -70,15 +71,19 @@ function run(selfInstanceId) {
             params['Filter.2.Name'] = 'attachment.instance-id';
             params['Filter.2.Value.1'] = id;
             ec2.call('DescribeVolumes', params, function(err, result) {
+                if (err) throw err;
+                if (!result.volumeSet.item) throw new Error('Volume not found on instance ' + id + ' at ' + device);
                 var volume = result.volumeSet.item.volumeId;
                 var description = job.description + ' ' + device + ' ' + id;
                 ec2.call('CreateSnapshot', {VolumeId: volume, Description: description}, function(err, result) {
+                    if (err) throw err;
                     if (!result.Errors) {
                         var params = {};
                         params['Owner'] = 'self';
                         params['Filter.1.Name'] = 'description';
                         params['Filter.1.Value.1'] = description;
                         ec2.call("DescribeSnapshots", params, function(err, result) {
+                            if (err) throw err;
                             if (!result.Errors) {
                                 if (result.snapshotSet.item) {
                                     var snapshots = result.snapshotSet.item;
@@ -86,6 +91,7 @@ function run(selfInstanceId) {
                                         snapshots = _.sortBy(snapshots, function(snapshot) { return new Date(snapshot.startTime).getTime(); });
                                         // Delete oldest snapshot within the pool. Assumes pool is not already exceeded.
                                         ec2.call('DeleteSnapshot', {SnapshotId: snapshots[0].snapshotId}, function(err, result) {
+                                            if (err) throw err;
                                             // Do nothing
                                         });
                                     }
